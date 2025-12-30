a8077f93df501a52e95496ead3a73208d1f717ca
Aashi Sarbhai <aashi.sarbhai@gmail.com>
2025-12-30 17:34:01 +0530

    implementing backend

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,10 @@
+const {Scehma} = require('mongoose');
+
+const OrdersSchema=new Schema({
+    name: String,
+    qty:Number,
+    price:Number,
+    mode:String,
+});
+
+module.exports={OrdersSchema};
